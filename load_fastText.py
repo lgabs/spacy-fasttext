4861acaa19842d32817dd33e07e19108504785e4
Luan <luan.fernandes@redealumni.com>
2020-06-15 08:39:25 -0300

    update loading

--- load_fastText.py
+++ load_fastText.py
@@ -13,41 +13,48 @@ import random
 from spacy.language import Language
 
 
-TAG_MAP = {
-    'N': {'pos': 'NOUN'},
-    'V': {'pos': 'VERB'},
-    'J': {'pos': 'ADJ'}
-}
+TAG_MAP = {"N": {"pos": "NOUN"}, "V": {"pos": "VERB"}, "J": {"pos": "ADJ"}}
 
 TRAIN_DATA = [
-    ("Ik zie mooie dingen", {'tags': ['N', 'V', 'J', 'N']}),
-    ("Hij maakt goede muziek", {'tags': ['N','V', 'J', 'N']})
+    ("Eu vejo coisas bonitas", {"tags": ["N", "V", "N", "J"]}),
+    ("Ele faz música boa", {"tags": ["N", "V", "N", "J"]}),
 ]
 
+VECTORS_PATH = "vector/cc.pt.300.vec"
 
+@plac.annotations(
+    vectors_loc=("Path to .vec file", "positional", None, str),
+    lang=(
+        "Optional language ID (e.g. 'pt' for Portuguese). If not set, blank Language() will be used.",
+        "positional",
+        None,
+        str,
+    ),
+)
 def main(vectors_loc=None, lang=None):
-    
+
     if lang is None:
-        nlp = spacy.blank('nl')
+        nlp = Language()
     else:
         # create empty language class – this is required if you're planning to
         # save the model to disk and load it back later (models always need a
         # "lang" setting). Use 'xx' for blank multi-language class.
-        nlp = spacy.blank('nl')
-    with open('vector/wiki.nl.vec', 'rb') as file_:
+        nlp = spacy.blank(lang)
+    with open(VECTORS_PATH, "rb") as file_:
+        print("loading vectors...")
         header = file_.readline()
         nr_row, nr_dim = header.split()
         nlp.vocab.reset_vectors(width=int(nr_dim))
         for line in file_:
-            line = line.rstrip().decode('utf8')
-            pieces = line.rsplit(' ', int(nr_dim))
+            line = line.rstrip().decode("utf8")
+            pieces = line.rsplit(" ", int(nr_dim))
             word = pieces[0]
-            vector = numpy.asarray([float(v) for v in pieces[1:]], dtype='f')
+            vector = numpy.asarray([float(v) for v in pieces[1:]], dtype="f")
             nlp.vocab.set_vector(word, vector)  # add the vectors to the vocab
-    
-    
-    tagger = nlp.create_pipe('tagger')
+
+    tagger = nlp.create_pipe("tagger")
     # Add the tags. This needs to be done before you start training.
+    print("trainning tags...")
     for tag, values in TAG_MAP.items():
         tagger.add_label(tag, values)
     nlp.add_pipe(tagger)
@@ -60,14 +67,15 @@ def main(vectors_loc=None, lang=None):
         print(losses)
 
     # test the trained model
-    test_text = "ik wil mooie vrouwen"
+    test_text = "Eu desejo ouvir uma música muito boa"
     doc = nlp(test_text)
-    print('Tags', [(t.text, t.tag_, t.pos_) for t in doc])
+    print("Tags", [(t.text, t.tag_, t.pos_) for t in doc])
 
-    print("Saved model to", 'nl_model_tagger')
+    print("Saved mode to nl_model_tagger")
 
-    nlp.to_disk('/app/model')
+    nlp.to_disk("/app/model")
 
-if __name__ == '__main__':
-    
-    main()
+
+if __name__ == "__main__":
+
+    main(vectors_loc=VECTORS_PATH, lang="pt")
